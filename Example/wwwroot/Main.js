4494af835354d298c810197cb5b08859e74ce9ff
Денис Гуляев <dnk363@live.ru>
2020-01-08 22:03:25 +0600

    Fix login, register

---
+++ Example/wwwroot/Main.js
@@ -0,0 +1,32 @@
+﻿//Create short url
+document.getElementById("short").addEventListener("click", function () {
+    var longurl = '?id=' + document.getElementById('longurl').value;
+    fetch('/home/shorturl/' + longurl).then(
+        function (response) {
+            if (response.status !== 200) {
+                return
+            }
+
+            response.json().then(function (result) {
+                document.getElementById('result').innerHTML = result;
+            });
+        }
+    );
+});
+
+//Show Data
+if (document.getElementById("dataItem")) {
+    document.getElementById("dataItem").addEventListener("click", function () {
+        fetch('/home/data/').then(
+            function (response) {
+                if (response.status !== 200) {
+                    return;
+                }
+
+                response.text().then(function (data) {
+                    document.getElementById('body').innerHTML = data;
+                });
+            }
+        );
+    });
+}
